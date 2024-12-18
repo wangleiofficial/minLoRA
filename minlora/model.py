ea4ea8b7bc877586b1bebd8f0e88dfbf23d6c383
Lei Wang <798306069@qq.com>
2024-12-18 23:18:08 +0800

    Update model.py

--- minlora/model.py
+++ minlora/model.py
@@ -81,7 +81,7 @@ def apply_lora(layer, register=True, merge=False, lora_config=default_lora_confi
                 parametrize.register_parametrization(layer, attr_name, parametrization(layer))
     else:  # this will remove all parametrizations, use with caution
         if hasattr(layer, "parametrizations"):
-            for attr_name in layer.parametrizations.keys():
+            for attr_name in list(layer.parametrizations.keys()):
                 parametrize.remove_parametrizations(layer, attr_name, leave_parametrized=merge)
 
 
